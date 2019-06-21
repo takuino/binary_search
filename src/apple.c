ffb66c40f6ce2ad7510d87b6ec3ebc8de03949b6
taku inoue <inoue.t.bg@m.titech.ac.jp>
2019-06-21 15:38:57 +0900

    array,apple完成

--- src/apple.c
+++ src/apple.c
@@ -1,17 +1,34 @@
 #include <stdio.h>
 
+
+
 int n;
 int k;
 int A[100000];
 
 
 int main(){
-  int lb, ub;
+  int i, ub, lb;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+    int max;
+    max= A[0];
+    for(i = 0; i < n; i++){
+        if(A[i] > max) max = A[i];
+    }
+    lb = 0;
+    ub = max
+    while(ub - lb > 1){
+        int m = (lb + ub)/2;
+        int sum = 0
+        for(i = 0; i < n; i++){
+            sum += (A[i] + m - 1) / m;
+        }
+        if(sum <= k) ub = m;
+        else lb = m;
+        }
+printf("%d\n", ub);
   return 0;
 }
